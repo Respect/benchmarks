25ea52c71c5b377afbf4ea6b0413e6e4e6ba429f
Alexandre Gaigalas <alexandre@gaigalas.net>
2012-12-04 21:26:02 -0200

    Added sample as safe as Silex

--- RespectRest/index.php
+++ RespectRest/index.php
@@ -5,5 +5,5 @@ require_once __DIR__.'/../vendor/autoload.php';
 $router = new Respect\Rest\Router();
 
 $router->get('/hello/*', function ($name)  {
-    return "Hello $name";
+    return "Hello " . filter_var($name, FILTER_SANITIZE_STRING);
 });
